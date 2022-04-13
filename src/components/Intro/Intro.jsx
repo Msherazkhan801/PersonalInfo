76f282349fea25c550de5893cf704e3d14ea2f4c
sheraz khan <sheraz.0403889@talmeez.pk>
2022-04-13 19:52:39 +0500

    social done

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -48,7 +48,7 @@ const Intro = () => {
       <div className="i-right">
         <img src={Vector1} alt="" />
         <img src={Vector2} alt="" />
-        <img src={boy} alt="" />
+        <img src={boy} alt="" className="boy" />
         {/* animation */}
         <motion.img
           initial={{ left: "-36%" }}
